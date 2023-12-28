5cf04dabfbfef9921fbff515b1875a4948140325
SohamTilekar <sohamtilekar233@gmail.com>
2023-12-28 16:22:13 +0530

    Added Diffrent Write Options, Added TextClip, ColorClip, ImageSequenceClip, Data2ImageClip, ImageClip.

--- src/video/VideoClips.py
+++ src/video/VideoClips.py
@@ -1,17 +1,15 @@
-from ast import While
 from fractions import Fraction
 import os
 from pathlib import Path
-from pprint import pprint
-import time
-from typing import (Any, Callable, Self, Generator, overload, override,
+from typing import (Callable, override,
                     )
 from copy import copy as _copy
+from PIL import Image
 import ffmpegio
 # from ..Clip import Clip
 # from ..audio.AudioClip import AudioClip
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageColor, ImageFont, ImageDraw
 
 class Clip:...
 class AudioClip:...
@@ -31,6 +29,7 @@ class VideoClip(Clip):
         self.pos = lambda t: (0, 0)
         self.make_frame: Callable[..., np.ndarray] = lambda t: (_ for _ in ()).throw(Exception('Make Frame is Not Set.'))
         self.make_frame_pil = lambda t: (_ for _ in ()).throw(Exception('Make Frame pil is Not Set.'))
+        self.make_frame_any = lambda t: (_ for _ in ()).throw(Exception('Make Frame any is Not Set.'))
         self.relative_pos = False
 
     @property
@@ -97,13 +96,18 @@ class VideoClip(Clip):
     def set_make_frame_pil(self, func):
         self.make_frame_pil = func
 
+    def set_make_frame_any(self, func):
+        self.make_frame_any = func
+
     def set_fps(self, fps):
         self.fps = fps
     
 
 
-    def get_frame(self, t, is_pil=True):
-        if is_pil:
+    def get_frame(self, t, is_pil=None):
+        if is_pil is not None:
+            return self.make_frame_any(t)
+        elif is_pil == True:
             return self.make_frame_pil(t)
         else:
             return self.make_frame(t)
@@ -133,10 +137,8 @@ class VideoClip(Clip):
     def iterate_frames_array_t(self, fps: Num):
         time_dif = 1 / fps
         x = 0
-        print(f'{time_dif=}, {x=}, {self.duration=}')
         if self.duration is not None:
             while x <= self.duration:
-                print(x)
                 yield self.get_frame(x, is_pil=False)
                 x += time_dif
 
@@ -150,8 +152,6 @@ class VideoClip(Clip):
         
         video_np = np.asarray(tuple(self.iterate_frames_array_t(fps if fps else self.fps if self.fps else (_ for _ in ()
                                                                                                             ).throw(Exception('Make Frame is Not Set.')))))
-
-        print(video_np)
 
         ffmpeg_options = {
             'preset': preset,
@@ -173,12 +173,40 @@ class VideoClip(Clip):
                              show_log=True,
                              **ffmpeg_options)
 
+    def write_imagesequence(self, nameformat, fps=None, dir='.', logger='bar'):
+        frame_number = 0
+
+        def save_frame(frame, frame_number):
+            file_path = os.path.join(dir, str(frame_number)+nameformat)
+            frame.save(file_path)
+
+        if dir!='.' and not os.path.exists(dir):
+            os.makedirs(dir)
+
+        if fps:
+            for frame in self.iterate_frames_pil_t(fps):
+                save_frame(frame, frame_number)
+                frame_number += 1
+        else:
+            if self.fps and self.duration:
+                for frame in self.iterate_frames_pil_t(self.fps):
+                    save_frame(frame, frame_number)
+                    frame_number += 1
+            else:
+                print("Warning: FPS is not provided, and fps and duration are not set.")
+
+                for frame in self.itrate_all_frames_pil():
+                    save_frame(frame, frame_number)
+                    frame_number += 1
+
+    def to_ImageClip(self, t):
+        return Data2ImageClip(self.get_frame(t))
+
 class VideoFileClip(VideoClip):
     def __init__(self, filename, ffmpeg_options=None):
         super().__init__()
         video_data = ffmpegio.probe.video_streams_basic(filename)[0]
         self.clip = self._import_video_clip(filename, ffmpeg_options)
-        print('clip:  - ', self.clip, type(self.clip))
         self.fps: float = float(video_data['frame_rate'])
         self.size = (video_data['width'], video_data['height'])
         self.start = 0.0
@@ -187,49 +215,57 @@ class VideoFileClip(VideoClip):
         self.set_make_frame_pil(self.make_frame_pil_sub_cls)
 
     def _array2image(self):
-        if isinstance(self.clip, np.ndarray):
-            return Image.fromarray(self.clip)
-        elif isinstance(self.clip, Image.Image):
-            return self.clip
+        if isinstance(self.clip[0], np.ndarray):
+            new_clip = []
+            for frame in self.clip:
+                new_clip.append(Image.fromarray(frame))
+            self.clip = np.array(new_clip)
+        elif isinstance(self.clip[0], Image.Image):
+            ...
         elif self.clip is None:
             raise ValueError("Clip is not Set.")
         else:
             raise ValueError("Clip is not an image or numpy array")
 
     def _image2array(self):
-        if isinstance(self.clip, Image.Image):
-            return np.array(self.clip)
-        elif isinstance(self.clip, np.ndarray):
-            return self.clip
+        if isinstance(self.clip[0], Image.Image):
+            new_clip = []
+            for frame in self.clip:
+                new_clip.append(np.asarray(frame))
+            self.clip = np.array(new_clip)
+        elif isinstance(self.clip[0], np.ndarray):
+           ...
         elif self.clip is None:
             raise ValueError("Clip is not Set.")
         else:
             raise ValueError("Clip is not an image or numpy array")
 
-    def make_frame_sub_cls(self, t):
-        self._image2array()
+    def make_frame_any_sub_cls(self, t):
         frame_num = t*self.fps
-        return self.clip[int(frame_num) - 1]
+        return self.clip[int(frame_num)]
+
+    def make_frame_sub_cls(self, t):
+        frame_num = t*self.fps
+        return np.array(self.clip[int(frame_num)])
 
     def make_frame_pil_sub_cls(self, t):
-        self._array2image()
         frame_num = t*self.fps
-        return Image.fromarray(self.clip[int(frame_num) - 1])
+        return Image.fromarray(self.clip[int(frame_num)])
 
     @override
     def iterate_frames_array_t(self, fps: Num):
         frame_t_dif = (1 / fps)
         st_0 = 0.0
-        print(f'{self.duration=}')
+        self._image2array()
         while st_0 < self.duration if self.duration is not None else (_ for _ in ()).throw(Exception('Duration is Not Set.')):
             yield self.make_frame(st_0)
             st_0 += frame_t_dif
-            print(f'{st_0=}')
 
     @override
     def iterate_frames_pil_t(self, fps: Num):
         frame_t_dif = (1 / fps)
         st_0 = 0.0
+        self._array2image()
         while st_0 < self.duration if self.duration is not None else fps if fps else (_ for _ in ()).throw(Exception('Duration is Not Set.')):
             yield self.make_frame_pil(st_0)
             st_0 += frame_t_dif
@@ -238,20 +274,20 @@ class VideoFileClip(VideoClip):
         options = {
                             **(ffmpeg_options if ffmpeg_options else {})
         }
-        print(f'{options=}')
         return ffmpegio.video.read(file_name, show_log=True, **options)[1]
 
 class ImageClip(VideoClip):
     
-    def __init__(self, image: str | Path, fps: NumOrNone = None, duration: NumOrNone = None):
+    def __init__(self, image: str | Path | None = None, fps: NumOrNone = None, duration: NumOrNone = None):
         super().__init__()
-        self.image = self._import_image(image)
-        self.set_make_frame(self.make_frame_sub_cls)
-        self.set_make_frame_pil(self.make_frame_pil_sub_cls)
+        self.image = self._import_image(image) if image else None
+        self.set_make_frame_any(self.make_frame_any_image_clip)
+        self.set_make_frame(self.make_frame_image_clip)
+        self.set_make_frame_pil(self.make_frame_pil_image_clip)
         self.fps = fps
         self.duration = duration
         self.start = 0.0
-        self.end = None
+        self.end = self.duration
 
     def _import_image(self, image):
         return Image.open(image)
@@ -276,13 +312,30 @@ class ImageClip(VideoClip):
         else:
             raise ValueError("image is not an image or numpy array")
 
-    def make_frame_sub_cls(self, t):
+    def make_frame_image_clip(self, t):
         self._image2array()
         return self.image
 
-    def make_frame_pil_sub_cls(self, t):
+    def make_frame_any_image_clip(self, t):
+        return self.image
+
+    def make_frame_pil_image_clip(self, t):
         self._array2image()
         return self.image
+
+class Data2ImageClip(ImageClip):
+    def __init__(self, data: np.ndarray | Image.Image, fps: NumOrNone = None, duration: NumOrNone = None):
+        super().__init__(fps=fps, duration=duration)
+        self.image = self._import_image(data)
+
+    @override
+    def _import_image(self, image):
+        if isinstance(image, np.ndarray):
+            return Image.fromarray(image)
+        elif isinstance(image, Image.Image):
+            return image
+        else:
+            raise TypeError(f"{type(image)} is not an Image.Image or numpy array Type.")
 
 class ImageSequenceClip(VideoClip):
     def __init__(self, images: str | Path | list[str | Path | Image.Image | np.ndarray] | np.ndarray, fps: NumOrNone = None):
@@ -351,20 +404,40 @@ class ImageSequenceClip(VideoClip):
     def iterate_frames_array_t(self, fps: Num):
         frame_t_dif = (1 / fps)
         st_0 = 0.0
-        print(f'{self.duration=}')
         while st_0 < self.duration if self.duration is not None else (_ for _ in ()).throw(Exception('Duration is Not Set.')):
             yield self.make_frame(st_0)
             st_0 += frame_t_dif
-            print(f'{st_0=}')
 
     @override
     def iterate_frames_pil_t(self, fps: Num):
         frame_t_dif = (1 / fps)
         st_0 = 0.0
-        while st_0 < self.duration if self.duration is not None else fps if fps else (_ for _ in ()).throw(Exception('Duration is Not Set.')):
+        while st_0 < self.duration if self.duration is not None else (_ for _ in ()).throw(Exception('Duration is Not Set.')):
             yield self.make_frame_pil(st_0)
             st_0 += frame_t_dif
 
+class ColorClip(Data2ImageClip):
+    def __init__(self, color: str | tuple[int, ...], mode='RGBA', size=(1, 1), fps=None, duration=None):
+        data = Image.new(mode, size, color) # type: ignore
+        super().__init__(data, fps=fps, duration=duration)
+
+class TextClip(Data2ImageClip):
+    def __init__(self, text: str, font_pth: None | str = None, font_size: int = 20, txt_color: str | tuple[int, ...]=(255, 255, 255), 
+                 bg_color: str | tuple[int,...] = (0, 0, 0, 0), fps=None, duration=None):
+        font = ImageFont.truetype(font_pth, font_size) if font_pth else ImageFont.load_default(font_size)
+        
+        # Get the bounding box of the text
+        bbox = font.getbbox(text)
+        # Calculate the image size to fit the text with some padding
+        image_width = bbox[2] + bbox[0] # + 20  # Adding 20 pixels padding
+        image_height = bbox[3] + bbox[1] # + 20  # Adding 20 pixels padding
+        image = Image.new("RGBA", (image_width, image_height), bg_color) # type: ignore
+        
+        # Draw the text on the image
+        draw = ImageDraw.Draw(image)
+        draw.text((0, 0), text, font=font, align='center', fill=txt_color) # type: ignore
+
+        super().__init__(image, fps=fps, duration=duration)
 
 if __name__ == '__main__':
     ...
