0c6c45365aaf76c1c2fa9dba29e35cf1a338e8b2
chengxs <chengxs@addcn.com>
2023-12-29 06:17:40 +0000

    Grammar modification compatible with version 3.11 of Python

--- src/video/VideoClips.py
+++ src/video/VideoClips.py
@@ -1,8 +1,8 @@
 from fractions import Fraction
 import os
 from pathlib import Path
-from typing import (Callable, override,
-                    )
+from typing import Callable
+
 from copy import copy as _copy
 from PIL import Image
 import ffmpegio
@@ -14,8 +14,8 @@ from PIL import Image, ImageColor, ImageFont, ImageDraw
 class Clip:...
 class AudioClip:...
 
-type Num = int | float
-type NumOrNone = Num | None
+Num = int | float
+NumOrNone = Num | None
 
 class VideoClip(Clip):
     def __init__(self) -> None:
@@ -244,8 +244,7 @@ class VideoFileClip(VideoClip):
         else:
             raise ValueError("Clip is not an image or numpy array")
 
-    @override
-    def fx(self, func, *args, **kwargs):
+    def fx(self, func: Callable, *args, **kwargs):
         clip = []
         for frame in self.itrate_all_frames_array():
             clip.append(func(frame, *args, **kwargs))
@@ -263,7 +262,6 @@ class VideoFileClip(VideoClip):
         frame_num = t*self.fps
         return Image.fromarray(self.clip[int(frame_num)])
 
-    @override
     def iterate_frames_array_t(self, fps: Num):
         frame_t_dif = (1 / fps)
         st_0 = 0.0
@@ -272,7 +270,6 @@ class VideoFileClip(VideoClip):
             yield self.make_frame(st_0)
             st_0 += frame_t_dif
 
-    @override
     def iterate_frames_pil_t(self, fps: Num):
         frame_t_dif = (1 / fps)
         st_0 = 0.0
@@ -303,8 +300,7 @@ class ImageClip(VideoClip):
     def _import_image(self, image):
         return Image.open(image)
 
-    @override
-    def fx(self, func, *args, **kwargs):
+    def fx(self, func: Callable, *args, **kwargs):
         self._array2image()
         self.image = func(self.image, *args, **kwargs)
         return self
@@ -345,7 +341,6 @@ class Data2ImageClip(ImageClip):
         super().__init__(fps=fps, duration=duration)
         self.image = self._import_image(data)
 
-    @override
     def _import_image(self, image):
         if isinstance(image, np.ndarray):
             return Image.fromarray(image)
@@ -417,7 +412,6 @@ class ImageSequenceClip(VideoClip):
             raise ValueError(f"Frame index {frame_index} exceeds the number of images in the sequence.")
         return self.images[frame_index]
 
-    @override
     def iterate_frames_array_t(self, fps: Num):
         frame_t_dif = (1 / fps)
         st_0 = 0.0
@@ -425,7 +419,6 @@ class ImageSequenceClip(VideoClip):
             yield self.make_frame(st_0)
             st_0 += frame_t_dif
 
-    @override
     def iterate_frames_pil_t(self, fps: Num):
         frame_t_dif = (1 / fps)
         st_0 = 0.0
