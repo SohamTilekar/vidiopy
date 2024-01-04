5ac33240bee060ea870e2394b508e395d4425c21
Soham Tilekar <sohamtilekar233@gmail.com>
2024-01-04 15:21:02 +0530

    Add author and license information to setup.py
    - Added author and author email to setup.py
    - Added URL to setup.py
    - Added license information to setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,14 @@ setup(
     version='0.0.60',
     description='An Video Editing Library Similar to Moviepy.',
     long_description=long_description,
+    author='Soham Tilekar',
+    author_email='sohamtilekar233@gamil.com',
+    # maintainer='Soham Tilekar',
+    # maintainer_email='sohamtilekar233@gmail.com',
+    url=r'https://github.com/SohamTilekar/vidiopy',
     package_dir={'': 'vidiopy'},
+    classifiers=['', '', ''],
+    license='MIT',
+    
     long_description_content_type='text/markdown',
-    url=r""
 )
