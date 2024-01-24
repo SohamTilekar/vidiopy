3477860b9cb637953c7f04c98e933aeb894815e0
Soham Tilekar <sohamtilekar233@gmail.com>
2024-01-24 16:50:07 +0530

    Add concatenate_videoclips function to VideoClips module

--- vidiopy/editor.py
+++ vidiopy/editor.py
@@ -3,7 +3,8 @@ from .audio.AudioClip import (AudioClip, AudioFileClip, CompositeAudioClip)
 from .Clip import Clip
 
 from .video.VideoClips import (VideoClip, VideoFileClip, ImageClip, Data2ImageClip,
-                               ImageSequenceClip, ColorClip, TextClip, CompositeVideoClip)
+                               ImageSequenceClip, ColorClip, TextClip, CompositeVideoClip,
+                               concatenate_videoclips)
 
 from .video.fx import *
 
