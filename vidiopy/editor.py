7e3e6d4fbbae8dd5d883766d1efdba38538d250b
Soham Tilekar <sohamtilekar233@gmail.com>
2024-01-26 17:17:12 +0530

    Refactor imports in editor.py

--- vidiopy/editor.py
+++ vidiopy/editor.py
@@ -1,13 +1,12 @@
+from .__version__ import __version__
+from .config import set_path, FFMPEG_BINARY, FFPROBE_BINARY
+from .video.fx import *
 from .audio.AudioClip import (AudioClip, AudioFileClip, CompositeAudioClip)
 
 from .Clip import Clip
 
-from .video.VideoClips import (VideoClip, VideoFileClip, ImageClip, Data2ImageClip,
-                               ImageSequenceClip, ColorClip, TextClip, CompositeVideoClip,
-                               concatenate_videoclips)
-
-from .video.fx import *
-
-from .config import set_path
-
-from .__version__ import __version__
+from .video.VideoClip import VideoClip
+from .video.VideoFileClip import VideoFileClip
+from .video.ImageSequenceClip import ImageSequenceClip
+from .video.mixing_clip import CompositeVideoClip, concatenate_videoclips
+from .video.ImageClips import ImageClip, ColorClip, TextClip, Data2ImageClip
