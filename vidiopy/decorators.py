12bdea396977e3cf1b9a0d194def7915e35d0df1
Soham Tilekar <sohamtilekar233@gmail.com>
2024-01-27 17:12:19 +0530

    remove the dependency of the decorator module

--- vidiopy/decorators.py
+++ vidiopy/decorators.py
@@ -1,53 +1,92 @@
-import decorator
-
-@decorator.decorator
-def requires_duration(f, clip, *a, **k):
-    """ Raise an error if the clip has no duration."""
-    
-    if clip.duration is None:
-        raise ValueError("Attribute 'duration' not set")
-    else:
-        return f(clip, *a, **k)
-
-# Make Decorator requires_size 
-@decorator.decorator
-def requires_size(f, clip, *a, **k):
-    """Raise an error if the clip has no size."""
-    if clip.size is None:
-        raise ValueError("Attribute 'size' not set")
-    else:
-        return f(clip, *a, **k)
-
-@decorator.decorator 
-def requires_fps(f, clip, *a, **k): 
-    """ Raise an error if the clip has no duration""" 
-    if clip.fps == None or 0: 
-        raise ValueError("Attribute 'fps' not set") 
-    else: 
-        return f(clip, *a, *k)
-
-@decorator.decorator 
-def requires_start(f, clip, *a, **k): 
-    """ Raise an error if the clip has no start.""" 
-    if clip.start is None: 
-        raise ValueError("Attribute 'start' not set") 
-    else: 
-        return f(clip, *a, **k)
-
-@decorator.decorator 
-def requires_end(f, clip, *a, **k): 
-    """ Raise an error if the clip has no end.""" 
-    if clip.end is None: 
-        raise ValueError(f"Attribute 'end' not set end={clip.end}") 
-    else: 
-        return f(clip, *a, **k)
-
-@decorator.decorator 
-def requires_start_end(f, clip, *a, **k): 
-    """ Raise an error if the clip has no start or end.""" 
-    if clip.start is None and clip.end is None: 
-        raise ValueError("Attribute 'start' or 'end' not set") 
-    else: 
-        return f(clip, *a, **k)
+from functools import wraps
 
 
+def requires_duration(f):
+    """ Decorator that raises an error if the clip has no duration."""
+
+    @wraps(f)
+    def wrapper(clip, *a, **k):
+        if not clip.duration:
+            raise ValueError("Attribute 'duration' not set")
+        else:
+            return f(clip, *a, **k)
+
+    return wrapper
+
+
+def requires_duration_or_end(f):
+    """ Decorator that raises an error if the clip has no duration or end."""
+
+    @wraps(f)
+    def wrapper(clip, *a, **k):
+        if not clip.duration and not clip.end:
+            raise ValueError("Attribute 'duration' or 'end' not set")
+        else:
+            return f(clip, *a, **k)
+
+    return wrapper
+
+
+def requires_size(f):
+    """ Decorator that raises an error if the clip has no size."""
+
+    @wraps(f)
+    def wrapper(clip, *a, **k):
+        if not clip.size:
+            raise ValueError("Attribute 'size' not set")
+        else:
+            return f(clip, *a, **k)
+
+    return wrapper
+
+
+def requires_fps(f):
+    """ Decorator that raises an error if the clip has no fps."""
+
+    @wraps(f)
+    def wrapper(clip, *a, **k):
+        if not clip.fps:
+            raise ValueError("Attribute 'fps' not set or `fps=0`")
+        else:
+            return f(clip, *a, **k)
+
+    return wrapper
+
+
+def requires_start(f):
+    """ Decorator that raises an error if the clip has no start."""
+
+    @wraps(f)
+    def wrapper(clip, *a, **k):
+        if not clip.start:
+            raise ValueError("Attribute 'start' not set")
+        else:
+            return f(clip, *a, **k)
+
+    return wrapper
+
+
+def requires_end(f):
+    """ Decorator that raises an error if the clip has no end."""
+
+    @wraps(f)
+    def wrapper(clip, *a, **k):
+        if not clip.end:
+            raise ValueError("Attribute 'end' not set")
+        else:
+            return f(clip, *a, **k)
+
+    return wrapper
+
+
+def requires_start_end(f):
+    """ Decorator that raises an error if the clip has no start or end."""
+
+    @wraps(f)
+    def wrapper(clip, *a, **k):
+        if not clip.start and not clip.end:
+            raise ValueError("Attribute 'start' or 'end' not set")
+        else:
+            return f(clip, *a, **k)
+
+    return wrapper
