26541cbc83729677f364b7faa0ee038829577633
Soham Tilekar <sohamtilekar233@gmail.com>
2024-01-28 14:00:02 +0530

    Add composite_videoclips function to mixing_clip.py

--- vidiopy/video/mixing_clip.py
+++ vidiopy/video/mixing_clip.py
@@ -6,6 +6,46 @@ from .ImageSequenceClip import ImageSequenceClip
 from .VideoClip import VideoClip
 
 
+def composite_videoclips(clips: Sequence[VideoClip],
+                         fps: int | float | None = None,
+                         bg_color: tuple[int, ...] = (0, 0, 0, 0),
+                         use_bg_clip: bool = False):
+    frames: list[Image.Image] = []
+    if use_bg_clip:
+        ...
+    else:
+        fps = fps if fps is not None else max(
+            clip.fps if clip.fps else 0.0 for clip in clips)
+        if not fps:
+            raise ValueError(
+                'Provide fps for clips or at least one clip or fps parameter for composite_videoclips')
+        td = 1/fps
+        duration: int | float = 0.0
+        max_size: tuple[int, int] = max(
+            tuple(clip.size if clip.size and clip.size[0] and clip.size[1]
+                  else (_ for _ in ()).throw(ValueError(f'Clip Size is not set, clip.__str__ = {clip.__str__()}')) for clip in clips))
+        for clip in clips:
+            clip_dur = clip.end - clip.start if clip.end else clip.duration - \
+                clip.start if clip.duration else None
+            if clip_dur and clip_dur > duration:
+                duration: int | float = clip_dur
+        if not duration:
+            raise ValueError('Provide duration for clips or at least one clip')
+        current_time = 0.0
+        while current_time <= duration:
+            current_frame = Image.new('RGBA', max_size, bg_color)
+            for clip in clips:
+                if clip.start <= current_time <= clip.end if clip.end else clip.duration if clip.duration else float('inf'):
+                    tmp_clip_frame = clip.make_frame_pil(current_time)
+                    current_frame.paste(
+                        tmp_clip_frame,
+                        clip.pos(current_time),
+                        tmp_clip_frame if tmp_clip_frame.has_transparency_data else None)
+            frames.append(current_frame)
+            current_time += td
+        return ImageSequenceClip(tuple(frames), fps=fps, duration=duration)
+
+
 def concatenate_videoclips(clips: tuple[VideoClip] | list[VideoClip],
                            transparent: bool = False,
                            fps: int | float | None = None,
