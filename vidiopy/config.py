0df312ab5a8a111317ed70299d79a314051e977e
Soham Tilekar <sohamtilekar233@gmail.com>
2024-01-29 15:22:31 +0530

    Fixed Major Bug

--- vidiopy/config.py
+++ vidiopy/config.py
@@ -3,8 +3,12 @@ import ffmpegio
 __all__ = ["FFMPEG_BINARY", "FFPROBE_BINARY", "set_path"]
 
 # Change Binary From Here
-FFMPEG_BINARY = ffmpegio.get_path()
-FFPROBE_BINARY = ffmpegio.get_path(probe=True)
+try:
+    FFMPEG_BINARY = ffmpegio.get_path()
+    FFPROBE_BINARY = ffmpegio.get_path(probe=True)
+except ffmpegio.path.FFmpegNotFound:
+    FFMPEG_BINARY = None
+    FFPROBE_BINARY = None
 
 
 def set_path(ffmpeg_path: str | None = None, ffprobe_path: str | None = None):
