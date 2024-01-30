f4317519bfc98902c218ce18cecb9f20b108b587
Soham Tilekar <sohamtilekar233@gmail.com>
2024-01-30 13:14:36 +0530

    Fix major error bug.

--- vidiopy/config.py
+++ vidiopy/config.py
@@ -12,6 +12,7 @@ except ffmpegio.path.FFmpegNotFound:
 
 
 def set_path(ffmpeg_path: str | None = None, ffprobe_path: str | None = None):
+    global FFMPEG_BINARY, FFPROBE_BINARY
     ffmpegio.set_path(ffmpeg_path, ffprobe_path)
     FFMPEG_BINARY = ffmpegio.get_path()
     FFPROBE_BINARY = ffmpegio.get_path(probe=True)
