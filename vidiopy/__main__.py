4d1af4ed6b91c4f64ddbcfdd0af111aac857e584
Soham Tilekar <sohamtilekar233@gmail.com>
2024-02-03 13:26:15 +0530

    Add conditional installation of ffmpeg

--- vidiopy/__main__.py
+++ vidiopy/__main__.py
@@ -1,1 +1,11 @@
-from .__init__ import *
+import sys
+
+if __name__ == "__main__":
+    if "--install_ffmpeg" in sys.argv:
+        from vidiopy.config import *
+
+        print("Installing ffmpeg...")
+        install_ffmpeg()
+        print("ffmpeg installed successfully!")
+    else:
+        from vidiopy.__init__ import *
