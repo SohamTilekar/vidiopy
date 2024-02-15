b0eaa88e9594a023abfee6b4e5e2e9c0b51294f5
Soham Tilekar <sohamtilekar233@gmail.com>
2024-02-15 13:06:50 +0530

    add new video effects to the __all__

--- vidiopy/video/fx/__init__.py
+++ vidiopy/video/fx/__init__.py
@@ -1,4 +1,25 @@
 from .accel_decel import accel_decel
 from .crop import crop
+from .filters import (
+    gaussian_blur,
+    box_blur,
+    unsharp_mask,
+    median_filter,
+    contrast,
+    brightness,
+    saturation,
+    sharpness,
+)
 
-__all__ = ["accel_decel", "crop"]
+__all__ = [
+    "accel_decel",
+    "crop",
+    "gaussian_blur",
+    "box_blur",
+    "unsharp_mask",
+    "median_filter",
+    "contrast",
+    "brightness",
+    "saturation",
+    "sharpness",
+]
