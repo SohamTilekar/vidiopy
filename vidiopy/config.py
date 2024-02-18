90d11ac7045a4278686ad43ed7a2f6546475073f
Soham Tilekar <sohamtilekar233@gmail.com>
2024-02-18 20:51:42 +0530

    Add function set_path to manage ffmpeg and ffprobe binary paths

--- vidiopy/config.py
+++ vidiopy/config.py
@@ -1,3 +1,5 @@
+"""This module manages the configuration of ffmpeg and ffprobe binaries."""
+
 import os
 import ffmpegio
 
@@ -36,6 +38,23 @@ except Exception:
 
 
 def set_path(ffmpeg_path: str | None = None, ffprobe_path: str | None = None):
+    """
+    Sets the paths for the ffmpeg and ffprobe binaries.
+
+    This function sets the paths for the ffmpeg and ffprobe binaries in the global variables FFMPEG_BINARY and FFPROBE_BINARY.
+    If the paths are not provided, it uses the default paths set in the ffmpegio library.
+
+    Parameters:
+    ffmpeg_path (str | None): The path to the ffmpeg binary. If None, the default ffmpeg path is used.
+    ffprobe_path (str | None): The path to the ffprobe binary. If None, the default ffprobe path is used.
+
+    Returns:
+    tuple: A tuple containing the paths to the ffmpeg and ffprobe binaries.
+
+    Raises:
+    RuntimeError: If Failed to auto-detect ffmpeg and ffprobe executable.
+    ValueError: If the given paths are not valid.
+    """
     global FFMPEG_BINARY, FFPROBE_BINARY
     ffmpegio.set_path(ffmpeg_path, ffprobe_path)
     FFMPEG_BINARY = ffmpegio.get_path()
