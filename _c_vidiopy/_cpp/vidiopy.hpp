26630cb8c4ed81f58e7bd3ce9c57aa5a1741441a
Soham Tilekar <sohamtilekar233@gmail.com>
2024-04-11 09:46:35 +0530

    Refactor VideoClip class in vidiopy.hpp to include setPos method and modify subClip and getFrame methods

--- _c_vidiopy/_cpp/vidiopy.hpp
+++ _c_vidiopy/_cpp/vidiopy.hpp
@@ -33,27 +33,37 @@ class VideoClip : public Clip
 private:
     struct PositionBundle
     {
-        char xChar;
-        int xInt;
-        double xDouble;
+        char xChar = '\0';
+        int xInt = 0;
+        double xDouble = 0.0;
 
-        char yChar;
-        int yInt;
-        double yDouble;
+        char yChar = '\0';
+        int yInt = 0;
+        double yDouble = 0.0;
     };
 
 public:
     int size[2];
     AudioClip *audio;
     bool relativePos;
+    std::function<PositionBundle(double)> Pos;
 
     void setAudio(AudioClip *audio);
     void withoutAudio();
     VideoClip *copy();
-    PositionBundle getPosition(double time);
-    virtual VideoClip subClip(double tStart, double tEnd);
-    virtual VideoClip subClipCopy(double tStart, double tEnd);
+    virtual VideoClip *subClip(double tStart, double tEnd);
+    virtual VideoClip *subClipCopy(double tStart, double tEnd);
+    virtual uint8_t ***getFrame(double time);
+    virtual uint8_t *getFrameFlattened(double time);
     void syncAudioVideoSED();
+    void setPos(int x, int y);
+    void setPos(double x, double y);
+    void setPos(char x, char y);
+    void setPos(int x, char y);
+    void setPos(char x, int y);
+    void setPos(double x, char y);
+    void setPos(char x, double y);
+    void setPos(std::function<PositionBundle(double)> func, bool relative = false);
     void writeVideoFile();
     void writeVideoFileSubclip();
     void writeImageSequence();
