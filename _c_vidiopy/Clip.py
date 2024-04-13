d44cbcff5018293edc8073886680f0580bf668f8
Soham Tilekar <sohamtilekar233@gmail.com>
2024-04-13 20:47:16 +0530

    Refactor Clip class in Clip.py to use float instead of double in setter methods

--- _c_vidiopy/Clip.py
+++ _c_vidiopy/Clip.py
@@ -22,9 +22,7 @@ class Clip:
         self.c_getStart.argtypes = [ctypes.POINTER(ctypes.c_void_p)]
         self.c_getStart.restype = ctypes.c_double
 
-        self.c_setEnd: Callable[[ctypes.c_void_p, ctypes.c_double], None] = (
-            lib.Clip_setEnd
-        )
+        self.c_setEnd: Callable[[ctypes.c_void_p, float], None] = lib.Clip_setEnd
         self.c_setEnd.argtypes = [ctypes.POINTER(ctypes.c_void_p), ctypes.c_double]
         self.c_setEnd.restype = None
 
@@ -32,7 +30,7 @@ class Clip:
         self.c_getEnd.argtypes = [ctypes.POINTER(ctypes.c_void_p)]
         self.c_getEnd.restype = ctypes.c_double
 
-        self.c_setDuration: Callable[[ctypes.c_void_p, ctypes.c_double], None] = (
+        self.c_setDuration: Callable[[ctypes.c_void_p, float], None] = (
             lib.Clip_setDuration
         )
         self.c_setDuration.argtypes = [ctypes.POINTER(ctypes.c_void_p), ctypes.c_double]
@@ -42,9 +40,7 @@ class Clip:
         self.c_getDuration.argtypes = [ctypes.POINTER(ctypes.c_void_p)]
         self.c_getDuration.restype = ctypes.c_double
 
-        self.c_setFps: Callable[[ctypes.c_void_p, ctypes.c_double], None] = (
-            lib.Clip_setFps
-        )
+        self.c_setFps: Callable[[ctypes.c_void_p, float], None] = lib.Clip_setFps
         self.c_setFps.argtypes = [ctypes.POINTER(ctypes.c_void_p), ctypes.c_double]
         self.c_setFps.restype = None
 
@@ -52,9 +48,7 @@ class Clip:
         self.c_getFps.argtypes = [ctypes.POINTER(ctypes.c_void_p)]
         self.c_getFps.restype = ctypes.c_double
 
-        self.c_setName: Callable[[ctypes.c_void_p, ctypes.c_char_p], None] = (
-            lib.Clip_setName
-        )
+        self.c_setName: Callable[[ctypes.c_void_p, str], None] = lib.Clip_setName
         self.c_setName.argtypes = [ctypes.POINTER(ctypes.c_void_p), ctypes.c_char_p]
         self.c_setName.restype = None
 
@@ -67,7 +61,7 @@ class Clip:
         ] = lib.Clip_setTimeTransforms
         self.c_setTimeTransforms.argtypes = [
             ctypes.POINTER(ctypes.c_void_p),
-            ctypes.py_object,
+            ctypes.CFUNCTYPE(ctypes.c_float, ctypes.c_float)
         ]
         self.c_setTimeTransforms.restype = None
 
@@ -84,4 +78,68 @@ class Clip:
 
     def set_start(self, value: float) -> Self:
         self.c_setStart(self.obj, value)
+        return self
+
+    @property
+    def end(self) -> float:
+        return self.c_getEnd(self.obj)
+
+    @end.setter
+    def end(self, value: float) -> None:
+        self.c_setEnd(self.obj, value)
+
+    def get_end(self) -> float:
+        return self.c_getEnd(self.obj)
+
+    def set_end(self, value: float) -> Self:
+        self.c_setEnd(self.obj, value)
+        return self
+
+    @property
+    def duration(self) -> float:
+        return self.c_getDuration(self.obj)
+
+    @duration.setter
+    def duration(self, value: float) -> None:
+        self.c_setDuration(self.obj, value)
+
+    def get_duration(self) -> float:
+        return self.c_getDuration(self.obj)
+
+    def set_duration(self, value: float) -> Self:
+        self.c_setDuration(self.obj, value)
+        return self
+
+    @property
+    def fps(self) -> float:
+        return self.c_getFps(self.obj)
+
+    @fps.setter
+    def fps(self, value: float) -> None:
+        self.c_setFps(self.obj, value)
+
+    def get_fps(self) -> float:
+        return self.c_getFps(self.obj)
+
+    def set_fps(self, value: float) -> Self:
+        self.c_setFps(self.obj, value)
+        return self
+
+    @property
+    def name(self) -> str:
+        return self.c_getName(self.obj)
+
+    @name.setter
+    def name(self, value: str) -> None:
+        self.c_setName(self.obj, value)
+
+    def get_name(self) -> str:
+        return self.c_getName(self.obj)
+
+    def set_name(self, value: str) -> Self:
+        self.c_setName(self.obj, value)
+        return self
+
+    def set_time_transforms(self, value: Callable[[float], float]) -> Self:
+        self.c_setTimeTransforms(self.obj, value)
         return self
